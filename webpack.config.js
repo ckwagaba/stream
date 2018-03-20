05c1669067f452076ebffc876e6d7439c32f0c11
CK Wagaba <ckwagaba@gmail.com>
2018-03-20 19:09:06 +0300

    install and configure TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: './src/index.js',
+  devtool: 'eval',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'stream.bundle.js'
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          {loader: "style-loader"},
-          {loader: "css-loader"}
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
+        exclude: /node_modules/
       },
       {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-          options: {minimize: true}
-        }
+        test: /\.css$/,
+        use: 'css-loader'
       }
     ]
   },
